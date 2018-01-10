dcf1c8e417bf5d692b618bd1e8b3da57896150f8
SaddamHusaain <saddamhusaainwins@gmail.com>
2018-01-10 13:22:22 +0530

    setup of angular

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HttpModule } from '@angular/http'
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './shared/modules/app-routing.module';
+import { AppBootstrapModule  } from './shared/modules/app-bootstrap.module';
 
 import { AppComponent } from './app.component';
 
+// Common header & footer component
+import { HeaderComponent } from './layout/header.component';
+import { FooterComponent } from './layout/footer.component';
+
+// Custom components
+import { MedicationsComponent } from './medications/medications.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        // Header & Footer component
+        FooterComponent,
+        HeaderComponent,
+
+        // Custom components
+        MedicationsComponent,
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        FormsModule,
+        AppRoutingModule,
+        AppBootstrapModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
